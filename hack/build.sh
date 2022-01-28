41850799a1971e9fa918b273ddfc21a728594d05
Pedro Duarte <pedro.dld536@gmail.com>
2022-01-28 18:11:49 +0000

    Update ports for deployment

--- hack/build.sh
+++ hack/build.sh
@@ -4,8 +4,8 @@ make create-volumes
 make build
 docker-compose up -d
 
-until ./wait-for-it.sh localhost:62081; do sleep 1; done
-until ./wait-for-it.sh localhost:62080; do sleep 1; done
+until ./wait-for-it.sh localhost:80; do sleep 1; done
+until ./wait-for-it.sh localhost:8080; do sleep 1; done
 
 until make bootstrap; do sleep 1; done
 
